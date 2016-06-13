2baf7785e448cc4e06b2066c7d75219da260eac2
Sander Maijers <S.N.Maijers@gmail.com>
2016-06-13 17:36:38 +0200

    Exit with correct status
    Pre-commit requires that the exit status is 1 in case an issue was found.

--- bin/run_lintr_on_file.R
+++ bin/run_lintr_on_file.R
@@ -3,6 +3,11 @@
 file_paths_cvec <- base::commandArgs(trailingOnly=TRUE)
 base::sink(file=stderr())
 for (file_path_str in file_paths_cvec) {
-	base::print(lintr::lint(file_path_str))
+	lints_lst <- lintr::lint(file_path_str)
+	base::print(lints_lst)
+	if (base::length(lints_lst) > 0L) {
+		base::options(error=NULL)
+		base::stop("Issues have been found. Please address them. ")
+	}
 }
 base::sink()
