c27132f6ea699053a6eef4cc643eae2a6e5fea5f
Sander Maijers <S.N.Maijers@gmail.com>
2016-06-13 17:14:28 +0200

    Print output to stderr

--- bin/run_lintr_on_file.R
+++ bin/run_lintr_on_file.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
 file_paths_cvec <- base::commandArgs(trailingOnly=TRUE)
+base::sink(file=stderr())
 for (file_path_str in file_paths_cvec) {
 	base::print(lintr::lint(file_path_str))
 }
+sink()
